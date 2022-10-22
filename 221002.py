d15b7c5592da2dcefe6168b57f6ee5cd47933e3b
HyunWookYooo <dbgusdnr6366@inha.edu>
2022-10-22 22:19:51 +0900

    second commit

--- 221002.py
+++ 221002.py
@@ -1,2 +1,16 @@
-$ pip install biopython
-import Bio
+from xml.sax.handler import DTDHandler
+
+
+A = "ATCGATCG"
+B =""
+for s in A:
+  if s == "A":
+    B += "T" 
+  elif s == "T": 
+    B += "A"
+  elif s == "G":
+    B += "C"
+  elif s == "C":
+    B += "G"
+    print(B[::-1]) 
+print(A)
